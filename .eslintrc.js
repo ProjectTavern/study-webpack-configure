dab714a815bb5980f2b4c15647e88e31d95692fa
qoov <emrys8128@gmail.com>
2018-05-07 15:00:29 +0900

    {qoov} Webpack Setting now ..

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    extends: ["eslint:recommended", "standard-preact"],
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: "module",
+        ecmaFeatures: {
+            experimentalObjectRestSpread: true,
+            jsx: true
+        }
+    },
+    plugins: [
+        "react"
+    ],
+    settings: {
+        react: {
+            pragma: 'h'
+        }
+    },
+    env: {
+        browser: true,
+        node: true,
+        jquery: true,
+        commonjs: true,
+        es6: true
+    }
+}
---
+++ webpack.config.babel.js
@@ -0,0 +1,52 @@
+import * as path from "path";
+
+
+const PATH_OUTPUT = path.join(__dirname, "/dist");
+const PATH_SOURCE = path.join(__dirname, "src");
+
+const ENV = process.env.PUB_ENV ? process.env.PUB_ENV : "local";
+const SOURCEPATH = process.env.npm_config_path;
+if ( !SOURCEPATH ) {
+    throw new Error("Require set the source path.");
+}
+
+let config = {
+    entry   : path.join(PATH_SOURCE, SOURCEPATH),
+    output  : {
+        path        : PATH_OUTPUT,
+        publicPath  : "",
+        filename    : "[name].bundle.js"
+    },
+    module  : {
+        rules   : [
+            {
+                enforce : "pre",
+                test    : /\.js$/,
+                exclude : /node_modules/,
+                include : /src/,
+                loader  : "eslint-loader",
+                options : {
+                    fix: true
+                }
+            },
+            {
+                test    : /\.js$/,
+                exclude : /node_modules/,
+                loader  : "babel-loader"
+            },
+            {
+                test    : /\.hbs/,
+                loader  : "html-loader!html-clean-loader"
+            }
+        ]
+    },
+    resolve : {
+        alias   : {
+            "react"     : "preact",
+            "react-dom" : "preact"
+        }
+    },
+    plugins : [],
+};
+
+export default config;
